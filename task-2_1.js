0de5b34ce5857afa112b90010b480a60df4e8062
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2023-07-20 19:16:58 +0300

    update code

--- task-2_1.js
+++ task-2_1.js
@@ -1,11 +1,15 @@
 const ADMIN_PASSWORD = 'jqueryismyjam'
-let message = prompt("Введіть пароль")
+const ask = prompt("Введіть пароль")
+let message
 
-if (message === ADMIN_PASSWORD) {
-    alert('Ласкаво просимо!')
-}else if (message === null) {
-    alert('Операцію скасовано!')
-}else if (message !== ADMIN_PASSWORD){
-    alert('Доступ заборонено, невірний пароль!')
+if (ask === null) {
+    message = 'Операцію скасовано!'
+} else {
+    if (ask === ADMIN_PASSWORD) {
+        message = 'Ласкаво просимо!'
+    } else {
+        message = 'Доступ заборонено, невірний пароль!'
+    }
 }
+alert(message)
 
--- task-2_2.js
+++ task-2_2.js
@@ -1,13 +1,18 @@
 let credits = 23580
 let price = 3000
-let howMany = prompt("«Скільки телевізорів бажаєте придбати?»")
-let totalPrice = howMany * price
-let balance = credits - totalPrice
+let howMany = prompt("«Скільки телевізорів бажаєте придбати?»") 
+let totalPrice
 
-if (howMany === null){
+if (howMany === null) {
     console.log("«Операцію скасовано!»")
-}else if (totalPrice > credits) {
-    console.log("«Недостатньо коштів на рахунку!»")
-}else{
-    console.log(`«Ви купили ${howMany} телевізорів, на рахунку залишилось ${balance} кредитів.»`)
+} else if (howMany < 1) {
+    console.log("«Невірно введені дані!»")
+} else {
+    totalPrice = howMany * price
+    if (totalPrice > credits) {
+        console.log("«Недостатньо коштів на рахунку!»")
+    } else {
+        credits = credits - totalPrice
+        console.log(`«Ви купили ${howMany} телевізорів, на рахунку залишилось ${credits} кредитів.»`)
+    }
 }
